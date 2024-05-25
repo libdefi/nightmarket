9e68df3a94b7c3b270aca9a1ddec63d89cf5ec95
libdefi <libcreatesweb3@gmail.com>
2024-05-25 14:31:22 +0800

    wip

--- frontend/src/components/Table/PredictionTable.tsx
+++ frontend/src/components/Table/PredictionTable.tsx
@@ -31,30 +31,49 @@ const PredictionTable: React.FC<PredictionTableProps> = ({ onSelectOutcome, sele
   };
 
   return (
-    <table className="table-auto w-full">
-      <thead>
-        <tr className="border-t border-b border-gray-300">
-          <th className="px-4 py-2 text-xs text-gray-500">OUTCOME</th>
-          <th className="px-4 py-2 text-xs text-gray-500">% CHANCE</th>
-          <th className="px-4 py-2"></th>
-          <th className="px-4 py-2"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <PredictionTableBody
-            key={index}
-            outcome={item.outcome}
-            chance={item.chance}
-            betYesAmount={item.betYesAmount}
-            betNoAmount={item.betNoAmount}
-            isSelected={selectedRow === index}
-            selectedBet={selectedRow === index ? selectedBet : null}
-            onSelectBet={(bet: 'YES' | 'NO') => handleSelectBet(index, bet)}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <table className="table-auto w-full mb-4">
+        <thead>
+          <tr className="border-t border-b border-gray-300">
+            <th className="px-4 py-2 text-xs text-gray-500">OUTCOME</th>
+            <th className="px-4 py-2 text-xs text-gray-500">% CHANCE</th>
+            <th className="px-4 py-2"></th>
+            <th className="px-4 py-2"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <PredictionTableBody
+              key={index}
+              outcome={item.outcome}
+              chance={item.chance}
+              betYesAmount={item.betYesAmount}
+              betNoAmount={item.betNoAmount}
+              isSelected={selectedRow === index}
+              selectedBet={selectedRow === index ? selectedBet : null}
+              onSelectBet={(bet: 'YES' | 'NO') => handleSelectBet(index, bet)}
+            />
+          ))}
+        </tbody>
+      </table>
+      <div className="my-8">
+        <h3 className="text-lg font-semibold border-b border-gray-200 pb-1 mb-2">Rules</h3>
+        <p className="text-sm text-gray-700">
+          Nic Carter (@nic__carter) and David Hoffman (@TrustlessState) are scheduled for a Karate Combat fight for May 30, 2024. 
+          If the fight is considered a draw, is canceled, or otherwise comes to an end without a definitive winner, this market will resolve Yes. 
+          If the fight between Nic Carter and David Hoffman, currently scheduled for May 30, 2024, is postponed to a date before August, this market&apos;s timeframe.
+        </p>
+      </div>
+      <div className="my-8">
+        <h3 className="text-lg font-semibold border-b border-gray-200 pb-1 mb-2">Activities</h3>
+        <div className="text-sm text-gray-700">
+          <p>0xEad515f64c8d5... sold 0.06 Yes for David Hoffman at 3.0¢ ($0)</p>
+          <p>0xEad515f64c8d5... sold 0.06 Yes for David Hoffman at 3.0¢ ($0)</p>
+          <p>0xEad515f64c8d5... sold 0.06 Yes for David Hoffman at 3.0¢ ($0)</p>
+          <p>0xEad515f64c8d5... sold 0.06 Yes for David Hoffman at 3.0¢ ($0)</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
