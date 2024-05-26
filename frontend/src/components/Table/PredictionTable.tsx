f08b14203c311ce5f0b98d4e4ffe8ecdd4fb83c4
libdefi <libcreatesweb3@gmail.com>
2024-05-26 23:00:41 +0800

    My positon

--- frontend/src/components/Table/PredictionTable.tsx
+++ frontend/src/components/Table/PredictionTable.tsx
@@ -7,6 +7,7 @@ interface PredictionTableProps {
     totalBet: string;
     odds: string;
   }>;
+  dataUser: readonly bigint[] | undefined;
   onSelectOutcome: (outcome: string, option: number) => void;
   selectedBet: 'YES' | 'NO';
   onSelectBet: (bet: 'YES' | 'NO') => void;
@@ -16,6 +17,7 @@ interface PredictionTableProps {
 
 const PredictionTable: React.FC<PredictionTableProps> = ({
   data,
+  dataUser,
   onSelectOutcome,
   selectedBet,
   onSelectBet,
@@ -32,6 +34,13 @@ const PredictionTable: React.FC<PredictionTableProps> = ({
     onSelectBet(bet);
     onSelectOutcome(data[index].outcome, index);
   };
+
+  const weiToEth = (wei: bigint): string => {
+    const weiString = wei.toString();
+    const ethValue = parseFloat(weiString) / 1e18;
+    return ethValue.toFixed(6);
+  };
+  
 
   return (
     <div>
@@ -59,6 +68,34 @@ const PredictionTable: React.FC<PredictionTableProps> = ({
           ))}
         </tbody>
       </table>
+
+      {dataUser && dataUser.length > 0 && (
+        <>
+        <h3 className="mt-8 text-md font-semibold pb-1 mb-2">My Bets</h3>
+        <table className="table-auto w-full mb-4">
+          <thead>
+            <tr className="border-t border-b border-gray-300">
+              <th className="px-4 py-2 text-xs text-gray-500">My position</th>
+              <th className="px-4 py-2 text-xs text-gray-500">Bet Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataUser.map((betAmount, index) => {
+              if (betAmount === BigInt(0)) {
+                return null;
+              }
+              return (
+                <tr key={index} className="border-b border-gray-200">
+                  <td className="px-4 py-2 text-left">{data[index].outcome}</td>
+                  <td className="px-4 py-2 text-center">{weiToEth(betAmount)} ETH</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        </>
+      )}
+
       <div className="my-8">
         <h3 className="text-lg font-semibold border-b border-gray-200 pb-1 mb-2">Rules</h3>
         <p className="text-md">
@@ -70,7 +107,7 @@ const PredictionTable: React.FC<PredictionTableProps> = ({
           The resolution will be conducted by the admin (address: ) based on the official statement from the Primodium X account (<a href="https://x.com/primodiumgame" className="text-blue-500 hover:underline">https://x.com/primodiumgame</a>), however a consensus of credible reporting might also be used.
         </p>
       </div>
-      <div className="my-8">
+      {/* <div className="my-8">
         <h3 className="text-lg font-semibold border-b border-gray-200 pb-1 mb-2">Activities</h3>
         <div className="text-sm ">
           <p>0xEad515f64c8d5... sold 0.06 Yes for David Hoffman at 3.0¢ ($0)</p>
@@ -78,7 +115,7 @@ const PredictionTable: React.FC<PredictionTableProps> = ({
           <p>0xEad515f64c8d5... sold 0.06 Yes for David Hoffman at 3.0¢ ($0)</p>
           <p>0xEad515f64c8d5... sold 0.06 Yes for David Hoffman at 3.0¢ ($0)</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
