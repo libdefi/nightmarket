503db11070dd4d05c07e48a3bc932548a8d0cc84
libdefi <libcreatesweb3@gmail.com>
2024-05-26 19:20:11 +0800

    Eliminate the round system,

--- contract/src/NightMarket.sol
+++ contract/src/NightMarket.sol
@@ -9,77 +9,29 @@ contract NightMarket is Ownable {
         mapping(address => uint256) bets;
     }
 
-    struct Round {
-        uint256 bettingEndTime;
-        uint256 resultDeclareTime;
-        uint256 totalPool;
-        uint256 returnRate;
-        bool eventEnded;
-        uint256 winningOption;
-        string[] optionNames;
-        mapping(uint256 => Option) options;
-        address creator;
-    }
-
-    uint256 public currentRoundId;
-    mapping(uint256 => Round) public rounds;
+    uint256 public bettingEndTime;
+    uint256 public resultDeclareTime;
+    uint256 public totalPool;
+    uint256 public returnRate;
+    bool public eventEnded;
+    uint256 public winningOption;
+    string[] public optionNames;
+    mapping(uint256 => Option) public options;
+    address public creator;
 
     event BetPlaced(
         address indexed bettor,
-        uint256 indexed round,
         uint256 indexed option,
         uint256 amount
     );
-    event ResultDeclared(uint256 indexed round, uint256 indexed winningOption);
+    event ResultDeclared(uint256 indexed winningOption);
 
-    constructor() Ownable(msg.sender) {}
-
-    modifier beforeBettingEnd(uint256 roundId) {
-        require(
-            block.timestamp < rounds[roundId].bettingEndTime,
-            "Betting period has ended"
-        );
-        _;
-    }
-
-    modifier afterBettingEnd(uint256 roundId) {
-        require(
-            block.timestamp >= rounds[roundId].bettingEndTime,
-            "Betting period has not ended yet"
-        );
-        _;
-    }
-
-    modifier beforeResultDeclare(uint256 roundId) {
-        require(
-            block.timestamp < rounds[roundId].resultDeclareTime,
-            "Result declare period has ended"
-        );
-        _;
-    }
-
-    modifier afterResultDeclare(uint256 roundId) {
-        require(
-            block.timestamp >= rounds[roundId].resultDeclareTime,
-            "Result declare period has not ended yet"
-        );
-        _;
-    }
-
-    modifier onlyCreatorOrOwner(uint256 roundId) {
-        require(
-            msg.sender == rounds[roundId].creator || msg.sender == owner(),
-            "Only the creator or owner can call this function"
-        );
-        _;
-    }
-
-    function startNewRound(
+    constructor(
         uint256 _bettingEndTime,
         uint256 _resultDeclareTime,
         uint256 _returnRate,
-        string[] calldata _optionNames
-    ) external {
+        string[] memory _optionNames
+    ) Ownable(msg.sender) {
         require(
             _bettingEndTime < _resultDeclareTime,
             "Betting end time should be before result declare time"
@@ -88,150 +40,92 @@ contract NightMarket is Ownable {
             _optionNames.length > 0,
             "At least one option must be provided"
         );
+        require(
+            _returnRate >= 95,
+            "Return rate should be at least 95"
+        );
 
-        currentRoundId++;
-        Round storage newRound = rounds[currentRoundId];
-        newRound.bettingEndTime = _bettingEndTime;
-        newRound.resultDeclareTime = _resultDeclareTime;
-        newRound.returnRate = _returnRate;
-        newRound.eventEnded = false;
+        bettingEndTime = _bettingEndTime;
+        resultDeclareTime = _resultDeclareTime;
+        returnRate = _returnRate;
+        eventEnded = false;
+        creator = msg.sender;
 
+        // Initialize optionNames array
         for (uint256 i = 0; i < _optionNames.length; i++) {
-            newRound.optionNames.push(_optionNames[i]);
+            optionNames.push(_optionNames[i]);
+            options[i].totalBets = 0;
         }
-
-        newRound.creator = msg.sender;
     }
 
-    function placeBet(
-        uint256 roundId,
-        uint256 option
-    ) external payable beforeBettingEnd(roundId) {
+    modifier beforeBettingEnd() {
+        require(block.timestamp < bettingEndTime, "Betting period has ended");
+        _;
+    }
+
+    modifier afterBettingEnd() {
+        require(
+            block.timestamp >= bettingEndTime,
+            "Betting period has not ended yet"
+        );
+        _;
+    }
+
+    modifier beforeResultDeclare() {
+        require(
+            block.timestamp < resultDeclareTime,
+            "Result declare period has ended"
+        );
+        _;
+    }
+
+    modifier afterResultDeclare() {
+        require(
+            block.timestamp >= resultDeclareTime,
+            "Result declare period has not ended yet"
+        );
+        _;
+    }
+
+    modifier onlyCreatorOrOwner() {
+        require(
+            msg.sender == creator || msg.sender == owner(),
+            "Only the creator or owner can call this function"
+        );
+        _;
+    }
+
+    function placeBet(uint256 option) external payable beforeBettingEnd {
         require(msg.value >= 0.01 ether, "Minimum bet is 0.01 ETH");
         require(
             msg.value % 0.01 ether == 0,
             "Bet amount must be in increments of 0.01 ETH"
         );
-        require(option < rounds[roundId].optionNames.length, "Invalid option");
+        require(option < optionNames.length, "Invalid option");
 
-        Round storage round = rounds[roundId];
-        Option storage o = round.options[option];
+        Option storage o = options[option];
         o.bets[msg.sender] += msg.value;
         o.totalBets += msg.value;
-        round.totalPool += msg.value;
+        totalPool += msg.value;
 
-        emit BetPlaced(msg.sender, roundId, option, msg.value);
+        emit BetPlaced(msg.sender, option, msg.value);
     }
 
     function declareResult(
-        uint256 roundId,
         uint256 _winningOption
-    )
-        external
-        onlyCreatorOrOwner(roundId)
-        afterBettingEnd(roundId)
-        beforeResultDeclare(roundId)
-    {
-        Round storage round = rounds[roundId];
-        require(_winningOption < round.optionNames.length, "Invalid option");
-        round.winningOption = _winningOption;
-        round.eventEnded = true;
-        emit ResultDeclared(roundId, _winningOption);
+    ) external onlyCreatorOrOwner afterBettingEnd beforeResultDeclare {
+        require(_winningOption < optionNames.length, "Invalid option");
+        winningOption = _winningOption;
+        eventEnded = true;
+        emit ResultDeclared(_winningOption);
     }
 
-    function claimReward(uint256 roundId) external afterResultDeclare(roundId) {
-        Round storage round = rounds[roundId];
-        require(round.eventEnded, "Event has not ended yet");
+    function claimReward() external {
+        require(eventEnded, "Event has not ended yet");
+        require(block.timestamp >= resultDeclareTime, "Result declare period has not ended yet");
 
-        Option storage o = round.options[round.winningOption];
+        Option storage o = options[winningOption];
         uint256 userBet = o.bets[msg.sender];
         require(userBet > 0, "No winning bet found");
 
-        uint256 reward = (round.totalPool * userBet) / o.totalBets;
-        o.bets[msg.sender] = 0;
-
-        payable(msg.sender).transfer(reward);
-    }
-
-    function getOptionTotalBets(
-        uint256 roundId,
-        uint256 option
-    ) external view returns (uint256) {
-        return rounds[roundId].options[option].totalBets;
-    }
-
-    function getUserBet(
-        uint256 roundId,
-        uint256 option,
-        address user
-    ) external view returns (uint256) {
-        return rounds[roundId].options[option].bets[user];
-    }
-
-    function getOptionNames(
-        uint256 roundId
-    ) external view returns (string[] memory) {
-        return rounds[roundId].optionNames;
-    }
-
-    function getRoundCreator(uint256 roundId) external view returns (address) {
-        return rounds[roundId].creator;
-    }
-
-    function getRoundInfo(
-        uint256 roundId
-    )
-        external
-        view
-        returns (
-            uint256 bettingEndTime,
-            uint256 resultDeclareTime,
-            uint256 totalPool,
-            uint256 returnRate,
-            bool eventEnded,
-            uint256 winningOption,
-            string[] memory optionNames,
-            address creator,
-            uint256[] memory totalBets,
-            uint256[] memory odds
-        )
-    {
-        Round storage round = rounds[roundId];
-        bettingEndTime = round.bettingEndTime;
-        resultDeclareTime = round.resultDeclareTime;
-        totalPool = round.totalPool;
-        returnRate = round.returnRate;
-        eventEnded = round.eventEnded;
-        winningOption = round.winningOption;
-        optionNames = round.optionNames;
-        creator = round.creator;
-
-        uint256 optionCount = round.optionNames.length;
-        totalBets = new uint256[](optionCount);
-        odds = new uint256[](optionCount);
-        for (uint256 i = 0; i < optionCount; i++) {
-            totalBets[i] = round.options[i].totalBets;
-            odds[i] =
-                (round.totalPool * 100) /
-                (
-                    round.options[i].totalBets == 0
-                        ? 1
-                        : round.options[i].totalBets
-                ); // Avoid division by zero
-        }
-    }
-
-    function canClaimReward(
-        uint256 roundId,
-        address user
-    ) external view returns (bool) {
-        Round storage round = rounds[roundId];
-        if (!round.eventEnded) {
-            return false;
-        }
-
-        Option storage o = round.options[round.winningOption];
-        uint256 userBet = o.bets[user];
-        return userBet > 0;
-    }
-}
+ 
--- contract/test/NightMarket.t.sol
+++ contract/test/NightMarket.t.sol
@@ -22,10 +22,79 @@ contract NightMarketTest is Test {
             110,
             options
         );
-        (uint256 bettingEndTime, , , , , , , , , ) = nightMarket.getRoundInfo(
-            1
-        );
+
+        (
+            uint256 bettingEndTime,
+            uint256 resultDeclareTime,
+            uint256 totalPool,
+            uint256 returnRate,
+            bool eventEnded,
+            uint256 winningOption,
+            string[] memory optionNames,
+            address creator,
+            uint256[] memory totalBets,
+            uint256[] memory odds
+        ) = nightMarket.getRoundInfo(1);
 
         assertEq(bettingEndTime, block.timestamp + 1 days);
+
+        uint256 optionCount = nightMarket.getOptionCount(1);
+        assertEq(optionCount, 2);
+
+        assertEq(
+            keccak256(bytes(optionNames[0])),
+            keccak256(bytes("Option 1"))
+        );
+        assertEq(
+            keccak256(bytes(optionNames[1])),
+            keccak256(bytes("Option 2"))
+        );
+    }
+
+    function testGetOptionCount() public {
+        string[] memory options = new string[](3);
+        options[0] = "Option A";
+        options[1] = "Option B";
+        options[2] = "Option C";
+
+        nightMarket.startNewRound(
+            block.timestamp + 1 days,
+            block.timestamp + 2 days,
+            120,
+            options
+        );
+
+        uint256 optionCount = nightMarket.getOptionCount(1);
+        assertEq(optionCount, 3);
+    }
+
+    function testGetOptionNames() public {
+        string[] memory options = new string[](3);
+        options[0] = "Option X";
+        options[1] = "Option Y";
+        options[2] = "Option Z";
+
+        nightMarket.startNewRound(
+            block.timestamp + 1 days,
+            block.timestamp + 2 days,
+            130,
+            options
+        );
+
+        string[] memory optionNames = nightMarket.getOptionNames(1);
+
+        assertEq(optionNames.length, 3);
+        assertEq(
+            keccak256(bytes(optionNames[0])),
+            keccak256(bytes("Option X"))
+        );
+        assertEq(
+            keccak256(bytes(optionNames[1])),
+            keccak256(bytes("Option Y"))
+        );
+        assertEq(
+            keccak256(bytes(optionNames[2])),
+            keccak256(bytes("Option Z"))
+        );
     }
 }
