23f50bec4e5410a2abe3c987835491f3d7b0b0c6
libdefi <libcreatesweb3@gmail.com>
2024-06-26 19:45:33 +0800

    Update optimize for cloudflare

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,31 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  
+  webpack: (config, { isServer }) => {
+    if (!isServer && process.env.NODE_ENV === 'production') {
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+        maxSize: 24000000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `npm.${packageName.replace('@', '')}`;
+            },
+          },
+        },
+      };
+    }
+    return config;
+  },
+
+  generateBuildId: async () => {
+    return `build-${Date.now()}`;
+  },
 }
 
 module.exports = nextConfig
