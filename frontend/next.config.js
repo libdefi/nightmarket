f1cd55c21e6d87f0bb0276415f71fa5c95c9b9e0
libdefi <libcreatesweb3@gmail.com>
2024-06-26 17:48:13 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
